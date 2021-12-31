1e1cebbb730560a73e999d874bc49338d29f2b5f
Aoyon123 <49103044+Aoyon123@users.noreply.github.com>
2021-12-31 15:44:46 +0600

    Create account vallidation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from "./logo.svg";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { createContext, useState } from "react";
@@ -6,31 +7,31 @@ import PrivateRoute from "./Components/PrivateRoute/PrivateRoute";
 import Profile from "./Components/Profile/Profile";
 import Login from "./Components/Login/Login";
 export const loggedInUser = createContext();
+
 function App() {
   const [loginUser, setLoginUser] = useState({});
   return (
-    <Router>
-      <loggedInUserProvider
-        value={[loginUser, setLoginUser]}
-      ></loggedInUserProvider>
-      <div>
-        <Switch>
-          <Route path="/about">
-            <h1>About</h1>
-          </Route>
+    <loggedInUser.Provider value={[loginUser, setLoginUser]}>
+      <Router>
+        <div>
+          <Switch>
+            <Route path="/about">
+              <h1>About</h1>
+            </Route>
 
-          <Route exact path="/">
-            <h1>Home</h1>
-          </Route>
-          <PrivateRoute path="/profile">
-            <Profile></Profile>
-          </PrivateRoute>
-          <Route path="/login">
-            <Login></Login>
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+            <Route exact path="/">
+              <h1>Home</h1>
+            </Route>
+            <PrivateRoute path="/profile">
+              <Profile></Profile>
+            </PrivateRoute>
+            <Route path="/login">
+              <Login></Login>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </loggedInUser.Provider>
   );
 }
 
--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,9 +1,114 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { useLocation } from "react-router-dom";
+import { useHistory } from "react-router-dom";
+import { loggedInUser } from "../../App";
 
 const Login = () => {
+  const [loginUser, setLoginUser] = useContext(loggedInUser);
+  const history = useHistory();
+  const location = useLocation();
+  const { from } = location.state || { from: { pathname: "/" } };
+  const [newUser, setNewUser] = useState(true);
+  const [user, setUser] = useState({
+    isLoggedIn: false,
+    fullName: "",
+    email: "",
+    dateOfBirth: "",
+    username: "",
+    password: "",
+  });
+  const handleForm = (e) => {};
+  const handleInput = (e) => {
+    let isFormValid = true;
+    if (e.target.name == "fullName") {
+      isFormValid = true;
+    }
+    if (e.target.name == "email") {
+      console.log(e.target.value);
+      if (validateEmail(e.target.value) == true) {
+        isFormValid = true;
+      } else {
+        alert("You have entered an invalid email address!");
+        isFormValid = false;
+      }
+    }
+    if (e.target.name == "dateOfBirth") {
+      isFormValid = true;
+    }
+    if (e.target.name == "username") {
+      isFormValid = true;
+    }
+    if (e.target.name == "password") {
+      if (e.target.value.length > 6) {
+        isFormValid = true;
+      } else {
+        alert("Please Type More Than 6 word");
+        isFormValid = false;
+      }
+    }
+    if (isFormValid) {
+      const newUser = { ...user };
+      newUser[e.target.name] = e.target.value;
+      setUser(newUser);
+    }
+  };
+  console.log(user);
+  const validateEmail = (email) => {
+    if (/^\w+([-]?\w+)*@\w+([-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+      return true;
+    }
+
+    return false;
+  };
+  console.log(loginUser);
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="m-5">
+      <div className="row d-flex justify-content-center ">
+        {newUser ? <h1>create an new account</h1> : <h1>Login</h1>}
+        <form action="" onSubmit={handleForm} className="form-group">
+          <label htmlFor="fullName">FullName</label>
+          <input
+            type="text"
+            className="form-control"
+            onBlur={handleInput}
+            name="fullName"
+            id="fullName"
+          />
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            className="form-control"
+            onBlur={handleInput}
+            name="email"
+            id="email"
+          />
+          <label htmlFor="dateOfBirth">Date of Birth</label>
+          <input
+            type="date"
+            className="form-control"
+            onBlur={handleInput}
+            name="dateOfBirth"
+            id="dateOfBirth"
+          />
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            className="form-control"
+            onBlur={handleInput}
+            name="username"
+            id="username"
+          />
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            className="form-control"
+            onBlur={handleInput}
+            name="password"
+            id="password"
+          />
+          <input type="submit" className="btn-primary" value="Submit" />
+        </form>
+      </div>
     </div>
   );
 };
